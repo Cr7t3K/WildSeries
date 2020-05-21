4239021c59c9a2730e25ab88572ad1efaf5178bb
Cr7t3K <cvr.contact.pro@gmail.com>
2020-05-21 17:23:38 +0200

    09 - Symfony : Les relations bidirectionnelles avec Doctrine

---
+++ README.md
@@ -0,0 +1,3 @@
+__**09 - Symfony : Les relations bidirectionnelles avec Doctrine**__
+
+[![IMAGE ALT TEXT](http://img.youtube.com/vi/tQt1eKkeRz8/0.jpg)](http://www.youtube.com/watch?v=tQt1eKkeRz8 "Video Title")
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,7 +34,7 @@ class WildController extends AbstractController
 
     /**
      * @Route("/wild/show/{slug}",
-     *     requirements={"slug"="[0-9-a-z]+$"},
+     *     requirements={"slug"="[\s/\w\-]+"},
      *     defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
      *     name="wild_show")
      * @param $slug
@@ -95,6 +96,74 @@ class WildController extends AbstractController
                 'currentCategory' => $categoryName
                 ]
         );
+    }
+
+    /**
+     * @Route("/wild/program/{slug}",
+     *     requirements={"slug"="[\s/\w\-]+"},
+     *     defaults={"slug"="Aucune série sélectionné"},
+     *     name="wild_program")
+     * @param $slug
+     * @return Response
+     */
+    public function showByProgram(string $slug) : Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        $seasons = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findBy(['program' => $program]);
+
+        return $this->render(
+            'wild/show.html.twig', [
+            'program' => $program,
+            'seasons' => $seasons
+        ]);
+    }
+
+    /**
+     * @Route("/wild/season/{id}",
+     *     name="wild_program_season")
+     * @param $id
+     * @return Response
+     */
+    public function showBySeason(int $id) :Response
+    {
+        if (!$id) {
+            throw $this
+                ->createNotFoundException('No id has been sent to find a season.');
+        }
+        $season = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findOneBy(['id' => $id]);
+
+        $program = $season->getProgram();
+        $episodes = $season->getEpisodes();
+
+        return $this->render(
+            'wild/show-season.html.twig', [
+                'season' => $season,
+                'program' => $program,
+                'episodes' => $episodes
+        ]);
+
     }
 
 }
